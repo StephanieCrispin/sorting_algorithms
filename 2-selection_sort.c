a44d5fb2c8de35b3a2d8b5f493cf92a284e14ef6
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-11-26 10:05:27 +0100

    worked on sorting tasks

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,42 +1,46 @@
 #include "sort.h"
+void swap_integers(int *a, int *b);
 
 /**
- * swap_integers -> swaps two integers
- * @a: Memory location of first integer
- * @b: Memory location of second integer
+ * selection_sort - Sort an array of integers in ascending order
+ *                  using the selection sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
  */
-
-void swap_integers(int *a, int *b)
-{
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-/***
- * selection-sort -> Sorts an array by checking for the
- * last item and swapping it with the ordered part of
- * array
- * @array: Th array to be sorted.
- * @size: The size of my array.
- */
-
 void selection_sort(int *array, size_t size)
 {
+    int *min;
+    size_t i, j;
 
-    size_t i, j, length = size;
-    size_t min_int;
-    if (!array || size < 2)
+    if (array == NULL || size < 2)
         return;
 
-    for (i = 0; i < (length - 1); i++)
+    for (i = 0; i < size - 1; i++)
     {
-        min_int = i;
-        for (j = (i + 1); j < length; j++)
-            if (array[j] < array[min_int])
-                min_int = j;
-        if (min_int != i)
-            swap_integers(&array[min_int], &array[i]);
-        print_array(array, size);
+        min = array + i;
+        for (j = i + 1; j < size; j++)
+            min = (array[j] < *min) ? (array + j) : min;
+
+        if ((array + i) != min)
+        {
+            swap_integers(array + i, min);
+            print_array(array, size);
+        }
     }
+}
+
+/**
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_integers(int *a, int *b)
+{
+    int tmp;
+
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
