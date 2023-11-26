2336805d1d02ffd9708ab39518f8cc05f115f918
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-11-26 09:30:59 +0100

    worked on sorting tasks

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -25,7 +25,7 @@ int lomuto_partition(int *array, size_t size, int left, int right)
         {
             if (above < below)
             {
-                swap_ints(array + below, array + above);
+                swap_integers(array + below, array + above);
                 print_array(array, size);
             }
             above++;
@@ -34,7 +34,7 @@ int lomuto_partition(int *array, size_t size, int left, int right)
 
     if (array[above] > *pivot)
     {
-        swap_ints(array + above, pivot);
+        swap_integers(array + above, pivot);
         print_array(array, size);
     }
 
