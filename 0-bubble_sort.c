834c1f50191c4477966354081b2129b2b2bee93d
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-11-26 09:25:24 +0100

    worked on sorting tasks

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -34,7 +34,7 @@ void bubble_sort(int *array, size_t size)
         {
             if (array[i] > array[i + 1])
             {
-                swap_integers(array[i], array[i + 1]);
+                swap_integers(&array[i], &array[i + 1]);
                 print_array(array, size);
                 swapped = false;
             }
